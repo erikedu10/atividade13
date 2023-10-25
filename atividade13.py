cf31448ac89bf5b6525a1bab3559a101912742f0
erikedu10 <148131588+erikedu10@users.noreply.github.com>
2023-10-25 10:43:24 -0300

    Add files via upload

---
+++ atividade13.py
@@ -0,0 +1,10 @@
+num1 = int(input("insira o primeiro número:"))
+num2 = int(input("insira o segundo número"))
+num3 = int(input("insira o terceiro número"))
+
+if num1 > num2:
+ print(num1)
+elif num2 > num1:
+ print(num2)
+else:
+ print(num3)
